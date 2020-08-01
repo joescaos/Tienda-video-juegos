570d6e7d2959bfe2e4119bcc6748c6ab04228cbe
Johan Cañas <jxexcxo@gmail.com>
2020-07-31 22:12:24 -0500

    fixing settings

--- playzoneshop/settings.py
+++ playzoneshop/settings.py
@@ -121,6 +121,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
